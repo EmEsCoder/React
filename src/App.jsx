41ce3907a51bb7e87cab4de1e0c1da02706c8b22
MGB <MGB@gmail.com>
2026-02-11 10:52:41 -0800

    c-css and inlineStyle and Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <H2 />
-      <p>salam</p>
+      <button className="rounded-md bg-cyan-500 px-4 py-2 text-sm font-semibold text-white opacity-100 focus:outline-none">salam</button>
     </>
   );
 }
--- src/Components/H2.jsx
+++ src/Components/H2.jsx
@@ -1,5 +1,15 @@
 function H2() {
-  return <h2>component hastam</h2>;
+  const ObjStyle = {
+    backgroundColor : "yellow",
+    fontSize : '3rem'
+  }
+
+  return (
+    <>
+      <h2 style={{color:'blue'}}>matn shomare 1</h2>;
+      <h1 style={ObjStyle}>matn shomare 2</h1>
+    </>
+  )
 }
 
 export default H2;
