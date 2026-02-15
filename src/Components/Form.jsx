8f72954465408b9eaedc09025af1505c4748903e
MGB <MGB@gmail.com>
2026-02-15 05:26:23 -0800

    C-checkbox

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,38 @@
 function Form() {
-    function print(e) {
-        console.log(e.target.value);
-        
-    }
+  function print(e) {
+    console.log(e.target.value);
+  }
+  let CheckItems = [];
+  function print2(e) {
+    // console.log(e.target.value, e.target.checked);
 
-    function selected(e) {
-        const value = [...e.target.selectedOptions].map((evo) => evo.value)
-        console.log(value);
-        
+    // const item = CheckItems.find((evo) => {
+    //   return evo === e.target.value;
+    // });
+    // if (item) {
+    //   CheckItems = CheckItems.filter((evo) => {
+    //      return evo !== e.target.value;
+    //   });
+    // } else {
+    //   CheckItems.push(e.target.value);
+    // }
+    // console.log(CheckItems);
+
+    if (e.target.checked) {
+      CheckItems.push(e.target.value);
+    } else {
+      CheckItems = CheckItems.filter((evo) => {
+        return evo !== e.target.value;
+      });
     }
+    console.log(CheckItems);
+    
+  }
+
+  function selected(e) {
+    const value = [...e.target.selectedOptions].map((evo) => evo.value);
+    console.log(value);
+  }
 
   return (
     <>
@@ -16,7 +40,7 @@ function Form() {
       <input
         type="text"
         onChange={(e) => {
-          console.log(e.target.value);
+          console.log(e.target.checked);
         }}
         className="ml-8 bg-red-200"
       />
@@ -31,8 +55,15 @@ function Form() {
       </select>
       <br />
       <br />
-      <input type="radio" name="gender" value="male"  onChange={print}/> Male
-      <input type="radio" name="gender" value="female" onChange={print}/> Female
+      <input type="radio" name="gender" value="male" onChange={print} /> Male
+      <input type="radio" name="gender" value="female" onChange={print} />{" "}
+      Female
+      <br />
+      <br />
+      <br />
+      <input type="checkbox" value="check1" onChange={print2} />
+      <input type="checkbox" value="check2" onChange={print2} />
+      <input type="checkbox" value="check3" onChange={print2} />
     </>
   );
 }
