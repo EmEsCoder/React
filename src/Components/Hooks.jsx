3113e403591806c668f207346607bfddb504f9cf
MGB <MGB@gmail.com>
2026-02-19 05:53:00 -0800

    C- fetchAPI useEffect

--- src/Components/Hooks.jsx
+++ src/Components/Hooks.jsx
@@ -1,31 +1,40 @@
 import { useEffect, useState } from "react";
 
 function sample() {
-  const [size, setSize] = useState(window.innerWidth);
+  const [posts, setPosts] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const checkSize = () => {
-    setSize(window.innerWidth);
-  };
+  async function placeHolder() {
+    try {
+      const nem = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!nem.ok) {
+        throw new Error("riiiiidiiiii");
+      } else {
+        const mem = await nem.json();
+        setPosts(mem);
+        setloading(false);
+      }
+    } catch (error) {
+      setError(error.message);
+      setloading(false);
+    }
+  }
 
   useEffect(() => {
-    console.log("useeffect");
-
-    window.addEventListener("resize", checkSize);
-
-    return () => {
-      console.log("cleanup");
-
-      window.removeEventListener("resize", checkSize);
-    };
-  },[]);
+    placeHolder();
+  }, []);
 
   return (
     <>
-      <h1>react.js - webprog.io</h1>
-      <h2>window: {size} px</h2>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>loading ...</h3>}
+      {posts &&
+        posts.map((evo) => {
+          return <h3 key={evo.id}>{evo.title}</h3>;
+        })}
     </>
   );
-
 }
 
 export default sample;
